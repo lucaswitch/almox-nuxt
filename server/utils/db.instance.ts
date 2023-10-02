45a1a5025ae704044937497ae3e72c67438ca51f
Lucas Alves <lucas.developing@gmail.com>
2023-10-02 00:47:40 -0300

    Adicionar produção.

--- server/utils/db.instance.ts
+++ server/utils/db.instance.ts
@@ -3,7 +3,7 @@ import {Sequelize} from "sequelize";
 // @ts-nocheck
 export const sequelize = new Sequelize({
     dialect: 'mysql',
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     database: process.env.MYSQL_DATABASE,
     username: process.env.MYSQL_DATABASE_USER,
     password: process.env.MYSQL_DATABASE_PASSWORD,
